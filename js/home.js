b31b0c979632183beef280bf03e60219fc25ca81
Ahmed Abd El-Ghany <ahmedabdoelhewala1211@gmail.com>
2022-05-07 23:09:20 +0200

    Updates

--- js/home.js
+++ js/home.js
@@ -331,6 +331,14 @@ $(document).on("click", function(e) {
         }
     }
 });
+// showing the go up btn
+window.onscroll = function() {
+    if (window.scrollY >= window.innerHeight) {
+        goUpBtn.classList.add("show");
+    } else {
+        goUpBtn.classList.remove("show");
+    }
+};
 // click on go up button
 goUpBtn.addEventListener("click", () => {
     window.scrollTo({
