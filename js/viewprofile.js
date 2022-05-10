255836a148af7eebdb1ebedda8e6fa8ac9fdebfd
Ahmed Abd El-Ghany <ahmedabdoelhewala1211@gmail.com>
2022-05-10 07:03:17 +0200

    Adding Updates

---
+++ js/viewprofile.js
@@ -0,0 +1,36 @@
+// page language value
+if (saveLanguage !== null) {
+    if (saveLanguage == "en") {
+        if (!$(langEnglish).hasClass("active")) {
+            $(langEnglish).siblings().removeClass("active");
+            $(langEnglish).addClass("active");
+            $("html").attr("lang", "en");
+            $("body").attr("translate", "no");
+            if ($(arabicStyle).attr("href") != undefined) {
+                $(arabicStyle).remove();
+            }
+        }
+    } else if (saveLanguage == "ar") {
+        if (!$(langArabic).hasClass("active")) {
+            $(langArabic).siblings().removeClass("active");
+            $(langArabic).addClass("active");
+            $("html").attr("lang", "ar");
+            $("body").attr("translate", "yes");
+            $(englishStyle).after(`<link rel="stylesheet" href="css/viewprofile-ar.css" class="style-ar" />`);
+        }
+    }
+}
+// End check Localstorage
+// click on arabic btn
+$(langArabic).on("click", function(e) {
+    if (!$(langArabic).hasClass("active")) {
+        $(langArabic).siblings().removeClass("active");
+        $(langArabic).addClass("active");
+        $("html").attr("lang", "ar");
+        $("body").attr("translate", "yes");
+        $(englishStyle).after(`<link rel="stylesheet" href="css/viewprofile-ar.css" class="style-ar" />`);
+        localStorage.setItem("page-language", "ar");
+    } else {
+        e.preventDefault();
+    }
+});
