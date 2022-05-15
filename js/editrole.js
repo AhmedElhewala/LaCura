cac799231ad93a24281cb4dcac10da9663eef74a
Ahmed Abd El-Ghany <ahmedabdoelhewala1211@gmail.com>
2022-05-15 03:13:17 +0200

    Adding Updates

---
+++ js/editrole.js
@@ -0,0 +1,41 @@
+// form variables
+let nameInput = `.user-form form div.name-section input.user-name`;
+let nameIcon = `.user-form form div.name-section .name-icon svg`;
+// page language value
+if (saveLanguage !== null) {
+    if (saveLanguage == "en") {
+        if (!$(langEnglish).hasClass("active")) {
+            $(langEnglish).siblings().removeClass("active");
+            $(langEnglish).addClass("active");
+            $("html").attr("lang", "en");
+            $("body").attr("translate", "no");
+            if ($(arabicStyle).attr("href") != undefined) {
+                $(arabicStyle).remove();
+            }
+        }
+    } else if (saveLanguage == "ar") {
+        if (!$(langArabic).hasClass("active")) {
+            $(langArabic).siblings().removeClass("active");
+            $(langArabic).addClass("active");
+            $("html").attr("lang", "ar");
+            $("body").attr("translate", "yes");
+            $(englishStyle).after(`<link rel="stylesheet" href="css/editrole-ar.css" class="style-ar" />`);
+        }
+    }
+}
+// End check Localstorage
+// click on arabic btn
+$(langArabic).on("click", function(e) {
+    if (!$(langArabic).hasClass("active")) {
+        $(englishStyle).after(`<link rel="stylesheet" href="css/editrole-ar.css" class="style-ar" />`);
+    }
+});
+// Toggle name icon by focusing
+$(nameInput).on({
+    focus: function() {
+        $(nameIcon).removeClass("fa-user").addClass("fa-user-pen");
+    },
+    blur: function() {
+        $(nameIcon).removeClass("fa-user-pen").addClass("fa-user");
+    }
+});
