b8b5df180aa0128bee9c62d0b39705e0cdd33a45
Ahmed Abd El-Ghany <ahmedabdoelhewala1211@gmail.com>
2022-05-18 07:52:32 +0200

    Updates

--- js/createdisease.js
+++ js/createdisease.js
@@ -1,12 +1,16 @@
 // form variables
-let diseaseInput = `.user-form form div.text-section div.input-section input.disease-name`;
-let diseaseIcon = `.user-form form div.text-section div.input-section .input-icon svg`;
+let diseaseInput = `.user-form form div.name-section div.input-section input`;
+let diseaseIcon = `.user-form form div.name-section div.input-section .input-icon svg`;
+let dealInput = `.user-form form div.area-section div.input-section textarea`;
+let dealIcon = `.user-form form div.area-section div.input-section .input-icon svg`;
 let petInput = `.user-form form div.pet-section div.input-section select.disease-pet`;
 let petIcon = `.user-form form div.pet-section div.input-section .select-icon svg`;
 let medicineInput = `.user-form form div.medicine-section div.input-section select.disease-medicine`;
 let medicineIcon = `.user-form form div.medicine-section div.input-section .select-icon svg`;
 let symptomInput = `.user-form form div.symptom-section div.input-section select.disease-symptom`;
 let symptomIcon = `.user-form form div.symptom-section div.input-section .select-icon svg`;
+let keywordInput = `.user-form form div.keyword-section div.input-section input`;
+let keywordIcon = `.user-form form div.keyword-section div.input-section .input-icon svg`;
 // End Variables
 // Start check Localstorage
 // page language value
@@ -35,14 +39,7 @@ if (saveLanguage !== null) {
 // click on arabic btn
 $(langArabic).on("click", function(e) {
     if (!$(langArabic).hasClass("active")) {
-        $(langArabic).siblings().removeClass("active");
-        $(langArabic).addClass("active");
-        $("html").attr("lang", "ar");
-        $("body").attr("translate", "yes");
         $(englishStyle).after(`<link rel="stylesheet" href="css/createdisease-ar.css" class="style-ar" />`);
-        localStorage.setItem("page-language", "ar");
-    } else {
-        e.preventDefault();
     }
 });
 // Toggle name icon by focusing
@@ -52,6 +49,15 @@ $(diseaseInput).on({
     },
     blur: function() {
         $(diseaseIcon).removeClass("fa-file-signature").addClass("fa-file");
+    }
+});
+// Toggle deal icon by focusing
+$(dealInput).on({
+    focus: function() {
+        $(dealIcon).removeClass("fa-file").addClass("fa-file-signature");
+    },
+    blur: function() {
+        $(dealIcon).removeClass("fa-file-signature").addClass("fa-file");
     }
 });
 // Toggle pet icon by focusing
@@ -79,5 +85,14 @@ $(symptomInput).on({
     },
     blur: function() {
         $(symptomIcon).removeClass("fa-box-open").addClass("fa-box");
+    }
+});
+// Toggle keyword icon by focusing
+$(keywordInput).on({
+    focus: function() {
+        $(keywordIcon).removeClass("fa-file").addClass("fa-file-signature");
+    },
+    blur: function() {
+        $(keywordIcon).removeClass("fa-file-signature").addClass("fa-file");
     }
 });
